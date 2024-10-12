afc65fd5d0893f69ec734d68228a18f44687b90b
鲁树人 <lu.shuren@um-react.app>
2024-10-13 05:54:12 +0900

    docs: added notes about mac client version requirements (close #70)

--- src/features/settings/panels/QMCv2/InstructionsMac.tsx
+++ src/features/settings/panels/QMCv2/InstructionsMac.tsx
@@ -1,12 +1,23 @@
-import { Heading, Text, Code, Kbd, OrderedList, ListItem } from '@chakra-ui/react';
+import { Heading, Text, Code, Kbd, OrderedList, ListItem, Link } from '@chakra-ui/react';
 import { FilePathBlock } from '~/components/FilePathBlock';
 import { MacCommandKey } from '~/components/Key/MacCommandKey';
 import { ShiftKey } from '~/components/Key/ShiftKey';
+
+const MAC_CLIENT_URL =
+  'https://web.archive.org/web/20230903/https://dldir1.qq.com/music/clntupate/mac/QQMusicMac_Mgr.dmg';
 
 export function InstructionsMac() {
   return (
     <>
       <Text>Mac 客户端使用 mmkv 数据库储存密钥。</Text>
+      <Text>
+        {'此外，你需要降级到 '}
+        <Link isExternal href={MAC_CLIENT_URL}>
+          2023.09.03 版本的客户端
+        </Link>
+        {'。'}
+        新版本对 mmkv 数据库进行了加密处理。
+      </Text>
       <Text>该密钥文件通常存储在下述路径：</Text>
       <FilePathBlock>
         ~/Library/Containers/com.tencent.QQMusicMac/Data/Library/Application Support/QQMusicMac/mmkv/MMKVStreamEncryptId
