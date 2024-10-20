77729cf653f1625cfa20fa59f644eb7323ee1373
鲁树人 <lu.shuren@um-react.app>
2024-10-21 08:00:51 +0900

    chore: bump version to v0.2.7

--- cmd/um/main.go
+++ cmd/um/main.go
@@ -34,7 +34,7 @@ import (
 	"unlock-music.dev/cli/internal/utils"
 )
 
-var AppVersion = "v0.2.6"
+var AppVersion = "v0.2.7"
 
 var logger, _ = logging.NewZapLogger() // TODO: inject logger to application, instead of using global logger
 
