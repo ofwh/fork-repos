5eccaf4173d0e343ac0f4287de04f30a18ac6ebf
mrFq1 <1xxbx0il0@mozmail.com>
2026-06-07 13:40:28 +0800

    misc: core md5 grep

--- install_dependency.sh
+++ install_dependency.sh
@@ -55,7 +55,7 @@ chmod +x com.metacubex.ClashX.ProxyConfigHelper.meta
 
 echo "Update meta core md5 to code"
 sed -i '' "s/WOSHIZIDONGSHENGCHENGDEA/$(md5 -q com.metacubex.ClashX.ProxyConfigHelper.meta)/g" ../ClashX/General/ClashProcess.swift
-grep -n "metaCoreMd5" ../ClashX/General/ClashProcess.swift
+grep -n "static let metaCoreMd5" ../ClashX/General/ClashProcess.swift
 
 echo "Gzip Universal core"
 gzip com.metacubex.ClashX.ProxyConfigHelper.meta
